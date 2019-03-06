f61455e46e947e4f819e5bb5b288de13892484b5
suantou <suantou@helijia.com>
2019-03-06 15:00:24 +0800

    linked

--- src/main/java/linked/反转链表.java
+++ src/main/java/linked/反转链表.java
@@ -1,46 +1,48 @@
-//package linked;
-//
-//import linked.base.ListNode;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * Created by JAVA on 2018/5/25.
-// */
-//public class 反转链表 {
-//
-//    public static void main(String[] args) {
-//        ListNode head = new ListNode();
-//
-//        for(int i=0;i<5;i++){
-//           head.add(i,i+1);
-//       }
-//
-//        reverseList(head);
-//
-//        System.out.println("---");
-//
-//
-//    }
-//
-//    public static ListNode reverseList(ListNode head) {
-//        List<Integer> stact = new ArrayList();
-//
-//        ListNode current = head;
-//        while(head!=null){
-//            stact.add(current.val);
-//            current=head.next;
-//        }
-//        current = head;
-//        int length = stact.size();
-//        for(int i=0;i<length ;i++){
-//            current.val= stact.get(i);
-//            current=current.next;
-//        }
-//
-//        return head;
-//    }
-//
-//
-//}
+package linked;
+
+import linked.base.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JAVA on 2018/5/25.
+ */
+public class 反转链表 {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode();
+
+
+
+        reverseList(head);
+
+        System.out.println("---");
+
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode top = null;
+        ListNode current = head;
+
+        while(current==null||current.next==null){
+            ListNode temp = current.next;
+            if(temp == null){
+                top = current;
+            }
+            temp.next =pre;
+
+            pre = current;
+            current = temp;
+
+
+        }
+
+
+        return top;
+    }
+
+
+}
